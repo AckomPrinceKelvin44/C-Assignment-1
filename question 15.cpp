7575b1b783d75567c259d9506916d115dfb156fd
AckomPrinceKelvin44 <princeackom444@gmail.com>
2026-06-14 23:40:35 +0000

    Add files via upload

---
+++ question 15.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main() {
+    string binary;
+    int decimal = 0;
+
+    cout << "Enter a binary number: ";
+    cin >> binary;
+
+    for (char bit : binary) {
+        decimal = decimal * 2 + (bit - '0');
+    }
+
+    cout << "Decimal = " << decimal << endl;
+    cout << "Octal = " << oct << decimal << endl;
+    cout << "Hexadecimal = " << hex << decimal << endl;
+
+    return 0;
+}
